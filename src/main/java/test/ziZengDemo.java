de2461c57a0330998ed761975f01e8c379de0b9f
Leetosun <leetosun@foxmail.com>
2017-08-17 11:57:34 +0800

    自增循环demo

---
+++ src/main/java/test/ziZengDemo.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * Created by LeeToSun on 2017/8/17 0017
+ */
+public class ziZengDemo {
+
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 10; i++) {
+            //The Value changed at 'a++' is never used
+            a = a++;
+        }
+        System.out.println(a);// 0
+    }
+
+}
